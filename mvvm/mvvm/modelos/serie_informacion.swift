6851781bf6756c6a78cba713ac627645812d56c6
Astrid C. Sanchez L <astridsanchez@amairani1221.uacj.mx>
2025-03-14 10:53:12 -0600

    [AGREGAR] Modelo y otras cosas

---
+++ mvvm/mvvm/modelos/serie_informacion.swift
@@ -0,0 +1,8 @@
+//
+//  aqui_guardan_sus_modelos.swift
+//  mvvm
+//
+//  Created by Astrid C. Sanchez L. on 3/14/25.
+//
+
+
---
+++ mvvm/mvvm/vistas_modelos/vista_modelo_basico.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  mvvm
+//
+//  Created by Astrid C. Sanchez L. on 3/14/25.
+//
+
+import Foundation
